3b3bf920999df0479c877ae800be4e95a81cb00f
Alinanealina <61042426+Alinanealina@users.noreply.github.com>
2024-07-02 20:53:17 +0700

    Add files via upload

---
+++ cl.c
@@ -0,0 +1,88 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+const int N = 1024;
+int f = 1;
+void rec_msg(int sock)
+{
+    int len;
+    char buf[N];
+    while (1)
+    {
+        if ((len = recv(sock, buf, N, 0)) < 0)
+        {
+            perror(" Плохое получение потоком.");
+            return;
+        }
+        buf[len] = '\0';
+        printf(" %s", buf);
+        if (strcmp("Отказано в доступе.", buf) == 0)
+        {
+            printf(" Нажмите Enter, чтобы выйти.");
+            fflush(stdout);
+            f = 0;
+            break;
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int sock;
+    char buf[N];
+    struct sockaddr_in servAddr;
+    struct hostent* hp, * gethostbyname();
+    pthread_t th;
+    pthread_attr_t ta;
+    if (argc < 3)
+    {
+        printf(" Ввести IP_сервера порт\n");
+        return 1;
+    }
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror(" Сокет не открылся.");
+        return 1;
+    }
+    bzero(&servAddr, sizeof(servAddr));
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(atoi(argv[2]));
+    hp = gethostbyname(argv[1]);
+    bcopy(hp->h_addr, &servAddr.sin_addr, hp->h_length);
+    pthread_attr_init(&ta);
+    pthread_attr_setdetachstate(&ta, PTHREAD_CREATE_DETACHED);
+    if (connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror(" Не удалось соединиться.\n");
+        return 1;
+    }
+
+    if (pthread_create(&th, &ta, (void*)rec_msg, (void*)sock) < 0)
+    {
+        perror(" Проблема с потоком.");
+        return 1;
+    }
+    printf(" Клиент готов.\n");
+
+    while (f)
+    {
+        bzero(buf, N);
+        fgets(buf, N, stdin);
+        if (send(sock, (const char*)buf, strlen(buf), 0) < 0)
+        {
+            perror(" Не удалось отправить.\n");
+            return 1;
+        }
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ serv.c
@@ -0,0 +1,143 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <signal.h>
+#include <pthread.h>
+#define M 3
+const int N = 1024;
+int n, s[M];
+pthread_mutex_t st_mutex;
+int add_sock(int sock)
+{
+    char* m = "Отказано в доступе.";
+    pthread_mutex_lock(&st_mutex);
+    if (n == M)
+    {
+        if (send(sock, (const char*)m, strlen(m), 0) < 0)
+            perror(" Не удалось отправить.");
+        else
+        {
+            printf(" Много клиентов.\n");
+            fflush(stdout);
+        }
+        pthread_mutex_unlock(&st_mutex);
+        return 0;
+    }
+    s[n++] = sock;
+    pthread_mutex_unlock(&st_mutex);
+    return 1;
+}
+
+void del_sock(int sock)
+{
+    int i, j;
+    pthread_mutex_lock(&st_mutex);
+    for (i = 0, j = 0; j < n; i++, j++)
+    {
+        if (s[i] == sock)
+            j++;
+        s[i] = s[j];
+    }
+    n--;
+    close(sock);
+    pthread_mutex_unlock(&st_mutex);
+}
+
+void msg(int sock)
+{
+    char buf[N], m[N + 12];
+    int i, len;
+    while (1)
+    {
+        bzero(buf, N);
+        len = recv(sock, buf, N, 0);
+        if (len < 0)
+        {
+            perror(" Плохое получение потоком.");
+            return;
+        }
+        else if (len == 0)
+        {
+            printf(" Пользователь %d ушел.\n", sock);
+            fflush(stdout);
+            break;
+        }
+        else
+        {
+            buf[len] = '\0';
+            sprintf(m, " Сокет %d: ", sock);
+            strcat(m, buf);
+            pthread_mutex_lock(&st_mutex);
+            for (i = 0; i < n; i++)
+            {
+                if ((s[i] != sock) && (send(s[i], (const char*)m, strlen(m), 0) < 0))
+                {
+                    perror(" Не удалось отправить.\n");
+                    return;
+                }
+            }
+            pthread_mutex_unlock(&st_mutex);
+        }
+    }
+    del_sock(sock);
+}
+
+int main()
+{
+    int sock_s, sock_c, len;
+    struct sockaddr_in servAddr;
+    pthread_t th;
+    pthread_attr_t ta;
+    if ((sock_s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror(" Сокет не открылся.");
+        return 1;
+    }
+    bzero(&servAddr, sizeof(servAddr));
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = 0;
+    if (bind(sock_s, (const struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror(" Сервер не удалось связать.");
+        return 1;
+    }
+    len = sizeof(servAddr);
+    if (getsockname(sock_s, (struct sockaddr*)&servAddr, &len))
+    {
+        perror(" Проблема с getsockname.");
+        return 1;
+    }
+    printf(" Номер порта сервера: %d\n", ntohs(servAddr.sin_port));
+    listen(sock_s, M);
+
+    pthread_attr_init(&ta);
+    pthread_attr_setdetachstate(&ta, PTHREAD_CREATE_DETACHED);
+    pthread_mutex_init(&st_mutex, 0);
+
+    while (1)
+    {
+        if ((sock_c = accept(sock_s, 0, 0)) < 0)
+        {
+            perror(" Проблема с сокетом клиента.");
+            return 1;
+        }
+        if (!add_sock(sock_c))
+            continue;
+        printf(" Socket для клиента: %d\n", sock_c);
+        fflush(stdout);
+        if (pthread_create(&th, &ta, (void*)msg, (void*)sock_c) < 0)
+        {
+            perror(" Проблема с потоком.");
+            return 1;
+        }
+    }
+    return 0;
+}
